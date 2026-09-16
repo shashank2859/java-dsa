8c212ab538f414ddb9077e6f676e620b21058308
shashank2859 <shashankh251@gmail.com>
2026-09-17 01:31:31 +0530

    Added palindrome using two pointers

---
+++ Strings/Palindrome.java
@@ -0,0 +1,28 @@
+package Strings;
+public class Palindrome {
+
+    static boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "madam";
+
+        System.out.println(isPalindrome(s));
+    }
+}
